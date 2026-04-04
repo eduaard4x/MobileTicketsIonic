b9fc9d205be2dc9adb7b46ba7246a6e7cd679790
eduaard4x <eduardadev@outlook.com>
2026-04-03 21:17:48 -0300

    implementação fila

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [CommonModule, IonicModule],
 })
 export class Tab1Page {
-  constructor() {}
+
+  filaSP: string[] = [];
+  filaSG: string[] = [];
+  filaSE: string[] = [];
+
+  ultimas: string[] = [];
+
+  contadorSP = 1;
+  contadorSG = 1;
+  contadorSE = 1;
+
+  gerarSP() {
+    this.filaSP.push('SP' + this.contadorSP++);
+  }
+
+  gerarSG() {
+    this.filaSG.push('SG' + this.contadorSG++);
+  }
+
+  gerarSE() {
+    this.filaSE.push('SE' + this.contadorSE++);
+  }
+
+  chamar() {
+    let senha: string | null = null;
+
+    if (this.filaSP.length > 0) {
+      senha = this.filaSP.shift()!;
+    } else if (this.filaSE.length > 0) {
+      senha = this.filaSE.shift()!;
+    } else if (this.filaSG.length > 0) {
+      senha = this.filaSG.shift()!;
+    }
+
+    if (senha) {
+      this.ultimas.unshift(senha);
+      if (this.ultimas.length > 5) {
+        this.ultimas.pop();
+      }
+    }
+  }
 }
